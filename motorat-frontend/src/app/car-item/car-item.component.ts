5444df48c1fe5d19ab58bee58f857ec4b4ff628d
Amin <sudanamin@hotmail.com>
2018-06-25 15:22:06 +0400

    remove what i was added

--- motorat-frontend/src/app/car-item/car-item.component.ts
+++ motorat-frontend/src/app/car-item/car-item.component.ts
@@ -95,14 +95,14 @@ getCarsThumbnail(){
     console.log(this.im[0].thums);
   }
   getCars(model,color){
-     this.allItems =[];
+    /*  this.allItems =[];
      this.pagedItems =[];
-     this.pager.pages = [];
+     this.pager.pages = []; */
     this.dataService.getCImages(this.ModelToSearch,this.ColorToSearch)
     .subscribe( cars => {
       this.allItems = cars;
       if(this.firstTime == true )this.setPage(1);
-      this.setPage(1);
+      //this.setPage(1);
       this.firstTime = false;
       this.getCarsThumbnail();
 
@@ -110,7 +110,7 @@ getCarsThumbnail(){
     })
   }
 
-  SearchCar(SearchFrm){
+  /*SearchCar(SearchFrm){
 
     //console.log('car id is :' + this.selectedCar.APPLICATION_ID);
      
@@ -122,10 +122,10 @@ getCarsThumbnail(){
       .subscribe(result => {
         console.log('original Item to be updated:' + result);
         this.getCars();
-      });*/
+      });
 
-    this.toggleForm = !this.toggleForm;
-  }
+   // this.toggleForm = !this.toggleForm;
+  }*/
 
   ngOnInit() {
     this.getCars('','');
