080da1acf9895e2c238bd8d795077c0c4cb4b1f0
sudanamin <sudanamin@hotmail.com>
2018-10-29 23:15:49 +0400

    some enhace in phtoswope fix of auto demention as per git

--- motorat-frontend/src/app/photo-swipe/photo-swipe.component.ts
+++ motorat-frontend/src/app/photo-swipe/photo-swipe.component.ts
@@ -3,18 +3,17 @@ import { Component, ViewChild, ElementRef, Input } from '@angular/core';
 // Import PhotoSwipe
 //mport PhotoSwipe           from 'photoswipe';
 //import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default';
-import * as PhotoSwipe from "photoswipe"; 
+import * as PhotoSwipe from "photoswipe";
 import * as PhotoSwipeUI_Default from "photoswipe/dist/photoswipe-ui-default"
 // Image Interface
 import { IImage } from '../interfaces/image';
 
 @Component({
-    selector   : 'app-photo-swipe',
+    selector: 'app-photo-swipe',
     templateUrl: './photo-swipe.component.html',
-    styleUrls  : ['./photo-swipe.component.css']
+    styleUrls: ['./photo-swipe.component.css']
 })
-export class PhotoSwipeComponent
-{
+export class PhotoSwipeComponent {
     @ViewChild('photoSwipe') photoSwipe: ElementRef;
 
     @Input() images: IImage[] = [];
@@ -23,8 +22,7 @@ export class PhotoSwipeComponent
     constructor() { }
 
     // ========================================================================
-    openGallery(images?,index?:number)
-    {
+    openGallery(images?, index?: number) {
         // Build gallery images array
         images = images || this.images;
 
@@ -37,21 +35,25 @@ export class PhotoSwipeComponent
 
         // Initializes and opens PhotoSwipe
         const gallery = new PhotoSwipe(this.photoSwipe.nativeElement, PhotoSwipeUI_Default, images, options);
-        gallery.listen('gettingData', function(index, item) {
-            if (item.w < 1 || item.h < 1) { // unknown size
-            var img = new Image(); 
-            img.onload = function() { // will get size after load
-            item.w = img.width; // set image width
-            item.h = img.height; // set image height
-               gallery.invalidateCurrItems(); // reinit Items
-               gallery.updateSize(true); // reinit Items
+        gallery.listen('gettingData', function (index, item) {
+            console.log('getting data');
+           /*  if (item.w < 1 || item.h < 1) { // unknown size */
+            if(item.html === undefined && item.onloading === undefined && (item.w < 1 || item.h < 1)) {
+                item.onloading = true;
+                var img = new Image();
+                img.onload = function () { // will get size after load
+                    console.log('image data ');
+                    item.w = img.width; // set image width
+                    item.h = img.height; // set image height
+                    gallery.invalidateCurrItems(); // reinit Items
+                    gallery.updateSize(true); // reinit Items
+                }
+                img.src = item.src; // let's download image
             }
-        img.src = item.src; // let's download image
-        }
-    });  
-        
+        });
+
         gallery.init();
-        
+
     }
     // ========================================================================
 }
